60132503d1c78b495a87c94da9d090a734bcf5d8
yequanrui <yequanrui@qq.com>
2024-04-05 19:42:55 +0800

    update

--- Hexagrams/Properties/AssemblyInfo.cs
+++ Hexagrams/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("FY")]
 [assembly: AssemblyProduct("Hexagrams made by VS2022(C#)")]
-[assembly: AssemblyCopyright("Copyright © 2011-2023")]
+[assembly: AssemblyCopyright("Copyright © 2011-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.8.1.0")]
+[assembly: AssemblyVersion("4.8.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
